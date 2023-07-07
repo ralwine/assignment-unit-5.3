f98a858cf2b84c70eb01f873e9816171af4837af
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-06 19:37:15 -0500

    got the first function working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,22 +3,35 @@ console.log('***** Music Collection *****')
 const collection = [];
 console.log(collection);
 
+// I want to make a function that takes multiple parameters as input,
+// converts them into an object with multiple properties and
+// adds it to an array.
 // Add a function named addToCollection. This function should:
 // - Take in the album's title, artist, yearPublished as input parameters
 function addToCollection(title, artist, yearPublished) {
     // - Create a new object having the above properties
-    console.log('Added new LP: ', title, artist, yearPublished);
+    console.log(title, artist, yearPublished);
     // - Add the new object to the end of the collection array
-    let newLp = collection[collection.push(title, artist, yearPublished)];
-    // - Return the newly created object
-    return newLp;
+    let newLp = {
+        "title": title,
+        "artist": artist,
+        "yearPublished": yearPublished,
+    }
+    return collection.push(newLp);
 }
-//function addToCollection('Pod', 'Breeders', 1990);
-//console.log(newLp);
+// - Return the newly created object
+
+addToCollection('"Pod"', 'Breeders', 1990);
+console.log(collection);
 
 // Test the addToCollection function:
 // - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. 
 //    (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+addToCollection('"Juju"', 'Siouxsie and the Banshees', 1982);
+console.log(collection);
+addToCollection('"Isn`t Anything"', 'My Bloody Valentine', 1988);
+console.log(collection);
+addToCollection('"Loveless"', 'My Bloody Valentine', 1991);
 // - Console.log each album as added using the returned value.
 // - After all are added, console.log the collection array.
 
