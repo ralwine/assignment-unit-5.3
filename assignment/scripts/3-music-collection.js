b78243b88a343d4d982cab5c31e7fa3932231e2c
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-07 09:19:50 -0500

    working on showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -41,22 +41,28 @@ addToCollection('"Zapp"', 'Zapp', 1980);
 console.log(collection);
 // - Console.log each album as added using the returned value.
 // - After all are added, console.log the collection array.
-
+console.log(collection);
+console.log (collection.length);
 // Add a function named showCollection. This function should:
-// - Take in an array parameter. (This allows it to be reused to show any collection, 
-//    like the results from the find or search.)
-// - Console.log the number of items in the array.
-// - Loop over the array and console.log each album's information formatted like: 
-//    TITLE by ARTIST, published in YEAR.
-
+function showCollection() {
+    // - Take in an array parameter. (This allows it to be reused to show any collection, 
+    //    like the results from the find or search.)
+    // - Console.log the number of items in the array.
+    console.log (collection.length);
+    // - Loop over the array and console.log each album's information formatted like: 
+    //    TITLE by ARTIST, published in YEAR.
+//    return title, ' by ', artist, ' published in ', year;
+}
 // Test the showCollection function.
+showCollection(collection);
 
 // Add a function named findByArtist. This function should:
+//findByArtist(artist) {
 // - Take in artist (a string) parameter
 // - Create an array to hold any results, empty to start
 // - Loop through the collection and add any objects with a matching artist to the array.
 // - Return the array with the matching results. If no results are found, return an empty array.
-
+//}
 // Test the findByArtist function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. 
 //  Check that for artists with multiple matches, all are found.
 
